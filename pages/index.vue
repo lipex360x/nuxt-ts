8090ce4a57f19718d4ff41957985bf5a39eaa0ee
Felipe Borges <lipex360@gmail.com>
2021-11-25 22:10:22 -0300

    head tags

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,19 @@
 import Vue from 'vue'
 export default Vue.extend({
   layout: 'website',
+
+  data() {
+    return {
+      title: 'Hello Nuxt'
+    }
+  },
+
+  head(this: any) {
+    return {
+      title: this.title
+    }
+  },
+
   mounted() {
     // this.$axios.$get('/post')
   }
